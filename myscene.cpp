a9f9a7821079298e5f5c0372f71a3097ca98d56b
feliperust1997 <felipefcarmo@poli.ufrj.br>
2016-02-19 19:25:03 -0200

    Added files via upload

--- myscene.cpp
+++ myscene.cpp
@@ -2,14 +2,32 @@
 #include <mypack.h>
 
 
+
+
+
+
+void MyScene::play_sound()
+{
+
+    card_sound->setMedia(QUrl("qrc:/sounds/card sound.mp3"));
+    card_sound->play();
+
+}
+
 void MyScene::draw()
 {
 
+   if (first_time){
+       desloc = 150;}
+   else{
+       desloc = desloc + 150;}
+
     //choose a random card
     MyPack * random_card = new MyPack();
-    random_card ->card->setPos(350,60);
+    random_card ->card->setPos(200 + desloc,60);
     //add the card to the scene
     addItem(random_card->card);
 
+    first_time = false;
 
 }
